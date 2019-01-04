4908798d7a7fdb7ebc99bece33a8829e847d775a
risva palintya <45660844+Risvapalintya@users.noreply.github.com>
2019-01-04 14:58:49 +0700

    Add files via upload

---
+++ menghitung rumus e=mc2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m ;
+    float c=3.00*10^8,E;
+    cout<<"masukan masa m"<<endl;
+    cin>>m;
+    E=m*c*c;
+    cout<<"rumus E adalah"<<E<endl;
+    return 0;
+}
